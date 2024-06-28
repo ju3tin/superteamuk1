5ebf47c9171c9ac3796db8bfd97bd99366257a98
Justin <ju3tin@hotmail.co.uk>
2024-06-28 09:23:23 +0100

    sd

--- pages/time/index.js
+++ pages/time/index.js
@@ -62,7 +62,7 @@ function Member() {
             'width': '100%',
             'height': '100%',
             'object-fit': 'cover',
-            'z-index': '-1'}} />
+            'z-index': '-1'}} /> 
    
       <Navcomponent currentPage={page}/>
     <Navcomponent1 currentPage={page} />
